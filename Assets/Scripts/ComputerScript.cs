39e427c3982bc892f83e203c4b80f0f0d041c4a1
Francesca Guzzi <76923530+francescaguzzi@users.noreply.github.com>
2023-03-14 20:03:11 +0100

    Updated ComputerScript
    Created the script for the AI player, enhanced the main player script

--- Assets/Scripts/ComputerScript.cs
+++ Assets/Scripts/ComputerScript.cs
@@ -2,17 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ComputerScript : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class ComputerScript : PlayerBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public Rigidbody2D pebble;
+
+
+    private void Update() {
+            
+        if (pebble.transform.position.y > 0.0f) {
+            // muovo il giocatore avversario in base alla posizione del sassolino
+            if (pebble.transform.position.x > this.transform.position.x) { // destra
+                    
+                Vector3 movement = new Vector3(1, 0.0f, 0.0f);
+                transform.position += movement * this.speed * Time.deltaTime;
+                flipPlayer(-1);
+            }
+            else if (pebble.transform.position.x < this.transform.position.x) { // sinistra
+                    
+                Vector3 movement = new Vector3(-1, 0.0f, 0.0f);
+                transform.position += movement * this.speed * Time.deltaTime;
+                flipPlayer(1);
+            }
+        } else {
+
+            // muovo il giocatore avversario in maniera randomica
+            if (this.transform.position.x > 0.0f) { // destra
+                Vector3 movement = new Vector3(-1, 0.0f, 0.0f);
+                transform.position += movement * this.speed * Time.deltaTime;
+                flipPlayer(-1);
+            }
+            else if (this.transform.position.x < 0.0f) { // sinistra
+                Vector3 movement = new Vector3(1, 0.0f, 0.0f);
+                transform.position += movement * this.speed * Time.deltaTime;
+                flipPlayer(1);
+            }
+
+        }   
+
     }
+     
 }
+    
+
--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -4,25 +4,23 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour {
     
-    public Rigidbody2D rigidBody;
+    protected Rigidbody2D rigidBody;
     public float speed = 9.0f;
 
     void Awake() {
+        
+        // recupero il componente rigidBody del giocatore
         rigidBody = GetComponent<Rigidbody2D>();
     }
 
     // funzione per invertire il giocatore a seconda della direzione
     public void flipPlayer (float moveHorizontal) {
 
-        // recupero i valori di scala attuali
-        float yScale = transform.localScale.y;
-        float xScale = transform.localScale.x;
-
-        if (moveHorizontal > 0) {
-            transform.localScale = new Vector3(-1, yScale, 1);
+        if (moveHorizontal > 0) { // destra
+            transform.localScale = new Vector3(-1, 1, 1);
         }
-        else if (moveHorizontal < 0) {
-            transform.localScale = new Vector3(1, yScale, 1);
+        else if (moveHorizontal < 0) { // sinistra
+            transform.localScale = new Vector3(1, 1, 1);
         }
     }
 
