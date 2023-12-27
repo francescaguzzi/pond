5b43c39b9967d64fab696cd68d8f02027273317c
Francesca Guzzi <fguzzi99@gmail.com>
2023-12-27 03:08:45 +0100

    speeded up background animation

--- Assets/Scripts/ParallaxScript.cs
+++ Assets/Scripts/ParallaxScript.cs
@@ -7,28 +7,22 @@ public class ParallaxScript : MonoBehaviour
     // script that slowly moves the background to create a parallax effect
     // right - left movement
 
-    private float speed = 0.5f;
+    private float speed = 1f;
     private Vector3 startPosition;
     private int direction = 1;
 
-    void Start()
-    {
+    void Start() {
         startPosition = transform.position;
     }
+    void Update() {
 
-    // Update is called once per frame
-    void Update()
-    {
         // max x position is 5, min x position is -5
         transform.position += Vector3.right * direction * speed * Time.deltaTime;
 
-        if (transform.position.x >= 4.9f)
-        {
-            direction = -1; // Inverti la direzione quando raggiungi il limite destro
-        }
+        if (transform.position.x >= 4.9f) 
+            direction = -1; 
         else if (transform.position.x <= -4.9f)
-        {
-            direction = 1; // Inverti la direzione quando raggiungi il limite sinistro
-        }
+            direction = 1; 
+        
     }
 }
